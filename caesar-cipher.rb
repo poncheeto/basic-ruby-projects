a9a73f26f17b77678e69433969f8ced7c248ea40
Poncho <jplyles1212@gmail.com>
2020-10-05 12:10:49 +0900

    Start to create methods to complete cipher

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,1 +1,13 @@
-puts "hi"
+# Create method to perform cipher
+
+def caesar_cipher(string, key)
+        shift_letters
+        return string
+    end
+
+
+#Create method to shift letters
+
+def shift_letters()
+    string.each_byte { |c| p c }
+end
