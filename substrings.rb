7f34b3a4b5d93b81d75a9f5a3cd68c84ed6c8245
Poncho <jplyles1212@gmail.com>
2020-10-08 08:30:41 +0900

    Add new exercise

---
+++ substrings.rb
@@ -0,0 +1,4 @@
+# Create the dictionay of accepted words
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit", "below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+# 
