fe0c90bdf0dcb96dfcd2c35479803728e3ee88e1
Poncho <jplyles1212@gmail.com>
2020-10-07 18:45:15 +0900

    Add rotate method to rotate from end of alphabet with uppercase

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -3,8 +3,14 @@ key = 0
 
 def caesar_cipher(string, key)
     alphabet = Array('a'..'z')
-    encrypter = Hash[alphabet.zip(alphabet.rotate(key))]
-    ciphered_text = string.chars.map { |c| encrypter.fetch(c, " ") }
+    lower = Hash[alphabet.zip(alphabet.rotate(key))]
+    
+    alphabet = Array('A'..'Z')
+    upper = Hash[alphabet.zip(alphabet.rotate(key))]
+
+    encrypter = lower.merge(upper)
+
+    ciphered_text = string.chars.map { |c| encrypter.fetch(c, c) }
     p ciphered_text.join
 
 end
