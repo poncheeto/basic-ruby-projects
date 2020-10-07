1fd57e815d5ebb1324df5d1a9f93e6f744f9a61e
Poncho <jplyles1212@gmail.com>
2020-10-08 08:51:07 +0900

    Add conditional to check if string is included in dictionary

--- substrings.rb
+++ substrings.rb
@@ -1,4 +1,10 @@
 # Create the dictionay of accepted words
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit", "below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-# 
+# Create method
+def substrings(string, array)
+    #Find if string in dictionary
+    if
+    array.include? string
+    end
+end
