e16547d4b6a856fd3cf4f5b59b79b19620d28485
Poncho <jplyles1212@gmail.com>
2020-10-09 07:27:31 +0900

    Add pseudocode

---
+++ stock-pciker.rb
@@ -0,0 +1,8 @@
+# Create method 
+def stock_picker(array)
+    # Iterate through array
+    array.map do |profit|
+        # Search array for highest integer
+            # Assign to variable labeled 'buy'
+        # Search array for lowest integer 
+            # Assign to variable labeled 'sell'
