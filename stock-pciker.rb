df2484e2e4b2e6baa969b9180a35999dd408d72c
Poncho <jplyles1212@gmail.com>
2020-10-09 16:16:49 +0900

    Assign buy and sell indices into profit variable

--- stock-pciker.rb
+++ stock-pciker.rb
@@ -1,17 +1,22 @@
 # Create method 
 def stock_picker(array)
-    # Search array for highest integer
+    # Search array for highest integer that's not first
     if array.first == array.max
         array[0] = array[1]
     end
-    # Assign to variable labeled 'buy'
-    buy = array.max
-    # Search array for lowest integer 
+    # Assign max to variable labeled 'buy'
+    sell = array.max
+    
+    # Search array for lowest integer that's not last
     if array.last == array.min
         array[-1] = array[-2]
     end
-    # Assign to variable labeled 'sell'
-    sell = array.min
+    # Assign min to variable labeled 'sell'
+    buy = array.min
+
+    # Assign buy/sell into an array labeled 'profit'
+    profit = [array.index(buy),array.index(sell)]
+    p profit
 end
 
 
