a72d01ef44ce70c7fc0f6fad2fb566436da41c95
Poncho <jplyles1212@gmail.com>
2020-10-09 15:15:41 +0900

    Move buy and sell variables out of if statement block

--- stock-pciker.rb
+++ stock-pciker.rb
@@ -1,19 +1,17 @@
 # Create method 
 def stock_picker(array)
     # Search array for highest integer
-        # Assign to variable labeled 'buy'
-        if array.first == array.max
-            array[0] = array[1]
-            buy = array.max
-        end
-        p buy
-        # Search array for lowest integer 
-            # Assign to variable labeled 'sell'
-        if array.last == array.min
-            array[-1] = array[-2]
-            sell = array.min
-        end
-        p sell
+    if array.first == array.max
+        array[0] = array[1]
+    end
+    # Assign to variable labeled 'buy'
+    buy = array.max
+    # Search array for lowest integer 
+    if array.last == array.min
+        array[-1] = array[-2]
+    end
+    # Assign to variable labeled 'sell'
+    sell = array.min
 end
 
 
