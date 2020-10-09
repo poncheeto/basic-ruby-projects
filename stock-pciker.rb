08a498491be31d3d1bdfc15087c62a9f97cc91e9
Poncho <jplyles1212@gmail.com>
2020-10-09 15:05:39 +0900

    Add conditionals to account for edge cases

--- stock-pciker.rb
+++ stock-pciker.rb
@@ -1,8 +1,20 @@
 # Create method 
 def stock_picker(array)
-    # Iterate through array
-    array.map do |profit|
-        # Search array for highest integer
-            # Assign to variable labeled 'buy'
+    # Search array for highest integer
+        # Assign to variable labeled 'buy'
+        if array.first == array.max
+            array[0] = array[1]
+            buy = array.max
+        end
+        p buy
         # Search array for lowest integer 
             # Assign to variable labeled 'sell'
+        if array.last == array.min
+            array[-1] = array[-2]
+            sell = array.min
+        end
+        p sell
+end
+
+
+        
