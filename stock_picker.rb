8365d8063083eb2979f845c844a763fd84c9424c
Poncho <jplyles1212@gmail.com>
2020-10-10 13:11:58 +0900

    Add another array to test and change comments

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 
 def stock_picker(array)
-  sell = if array.first == array.max # If highest price first, set second highest to sell variable
+  sell = if array.first == array.max # Check if max is first element
            array.max(2).last
          else
            array.max
          end
 
-  buy = if array.last == array.min # If lowest price last, set second lowest to buy variable
+  buy = if array.last == array.min # Check if min is last element
           array.min(2).last
         else
           array.min
         end
 
-  return unless array.index(buy) > array.index(sell)
+  return unless array.index(buy) > array.index(sell) # Check if buy index greater than sell index
 
   buy = array.min(2).last
   profit = array.index(buy), array.index(sell)
@@ -21,3 +21,6 @@ def stock_picker(array)
 end
 
 stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
+# => [1, 4]
+stock_picker([3, 10, 11, 12, 8, 7, 20, 1, 5])
+# => [0, 6]
