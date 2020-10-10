d75590cb428dc5b4a7e459a830428b135a142a60
Poncho <jplyles1212@gmail.com>
2020-10-10 12:13:38 +0900

    Refactor code to most of rubocop standards

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,28 +1,21 @@
 def stock_picker(array)
-    if array.first == array.max
-      # If highest price first, set second highest to sell variable
-      sell = array.max(2).last
-    else
-      sell = array.max
-    end
+  sell = if array.first == array.max # If highest price first, set second highest to sell variable
+           array.max(2).last
+         else
+           array.max
+         end
 
-    if array.last == array.min
-      # If lowest price last, set second lowest to buy variable
-      buy = array.min(2).last
-    else
-      buy = array.min
-    end
+  buy = if array.last == array.min # If lowest price last, set second lowest to buy variable
+          array.min(2).last
+        else
+          array.min
+        end
 
-    if array.index(buy) > array.index(sell)
-      # If index of low price greater than high price, set second lowest to buy variable
-      buy = array.min(2).last
-      # Assign buy/sell into an array labeled 'profit'
-      profit = array.index(buy),array.index(sell)
-      p profit
-    else
-      profit = array.index(buy),array.index(sell)
-      p profit
-    end
+  return unless array.index(buy) > array.index(sell)
+
+  buy = array.min(2).last
+  profit = array.index(buy), array.index(sell)
+  p profit
 end
 
-stock_picker([17,3,6,9,15,8,6,1,10])
+stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
