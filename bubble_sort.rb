b23ea7f2cd8fcee0f6379b8e0a82cffae3de0054
Poncho <jplyles1212@gmail.com>
2020-10-11 16:47:43 +0900

    Complete bubble_sort method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,9 +1,24 @@
 # frozen_string_literal: true
 
 def bubble_sort(array)
-    counter = -1
-    array.map 
+  array_length = array.size
+  return array if array_length <= 1 # Return array if it is sorted
+
+  loop do
+    swapped = false # To check if numbers have been swapped
+
+    (array_length - 1).times do |i|
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swapped = true
+      end
+    end
+
+    break unless swapped
+  end
+
+  array
 end
 
-bubble_sort([4,3,78,2,0,2])
-# => [0,2,2,3,4,78]
+p bubble_sort([4, 3, 78, 2, 0, 2])
+# => [0, 2, 2, 3, 4, 78]
