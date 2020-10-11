238dac719ee40c06d41ffb3147e494e77edc9308
Poncho <jplyles1212@gmail.com>
2020-10-11 13:42:36 +0900

    Create separate buy/sell methods and refactor stock_picker method

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,23 +1,27 @@
 # frozen_string_literal: true
 
 def stock_picker(array)
-  sell = if array.first == array.max # Check if max is first element
-           array.max(2).last
-         else
-           array.max
-         end
-
-  buy = if array.last == array.min # Check if min is last element
-          array.min(2).last
-        else
-          array.min
-        end
-
-  return unless array.index(buy) > array.index(sell) # Check if buy index greater than sell index
+  return unless array.index(buy(array)) > array.index(sell(array)) # Check if buy index greater than sell index
 
   buy = array.min(2).last
-  profit = array.index(buy), array.index(sell)
+  profit = array.index(buy), array.index(sell(array))
   p profit
+end
+
+def sell(array)
+  if array.first == array.max # Check if max is first element
+    array.max(2).last
+  else
+    array.max
+  end
+end
+
+def buy(array)
+  if array.last == array.min # Check if min is last element
+    array.min(2).last
+  else
+    array.min
+  end
 end
 
 stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
